c260a3df08d8e05a1917f0ca32c84265bc8f798d
jeongmino <jeongmino1207@gmail.com>
2023-01-08 15:00:27 +0900

    ft_putchar.c , main.c, put.h

---
+++ putchar/main.c
@@ -0,0 +1,9 @@
+#include "put.h"
+
+int main(void)
+{
+  ft_putchar('H');
+  ft_putchar('\n');
+  return (0);
+}
+
---
+++ putchar/put.h
@@ -0,0 +1,10 @@
+#ifndef PUT_H
+# define PUT_H
+
+#include <unistd.h>
+
+#define ZERO 48
+
+void ft_putchar(char c);
+
+#endif
